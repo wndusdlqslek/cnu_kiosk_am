6f57c2d78b540b9d9820b172a04b209d8aaf4715
wndusdlqslek <91604500+wndusdlqslek@users.noreply.github.com>
2021-11-02 23:36:41 +0900

    Update choice_menu.py

--- choice_menu.py
+++ choice_menu.py
@@ -1,5 +1,7 @@
 # 1.햄버거 세부 메뉴 선택하는 함수
-def choice_burger():
+def choice_burger(): ("치즈버거", "불고기버거", "새우버거") 
+for food in BUGER_MENU : 
+    print(MENU)
     print('■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■')
     print('□■  BURGER MENU')
     print('□■  1.치즈버거: 3,500원')
@@ -8,7 +10,9 @@ def choice_burger():
     print('■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■')
     print('■■ 원하시는 메뉴의 번호를 입력해주세요.')
 
-    while True:
+    while True: number <= 1:
+            print(number)
+            number += 1
         choice_num = int(input('>> 번호: '))
         if choice_num >= 1 and choice_num <= 3:
             break
@@ -18,7 +22,9 @@ def choice_burger():
 
 
 # 2.사이드 세부 메뉴 선택하는 함수
-def choice_side():
+def choice_side(): ("프렌치프라이", "치킨너겟")
+for side in Side_MENU :
+    print(MENU)
     print('■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■')
     print('□■  SIDE MENU')
     print('□■  1.프렌치프라이: 1,500원')
@@ -34,7 +40,9 @@ def choice_side():
     return choice_num2
 
 # 3.음료 세부 메뉴 선택하는 함수
-def choice_drink():
+def choice_drink(): ("코카코라","커피")
+for drink in Drink_MENU
+    print(MENU)
     print('■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■')
     print('□■  DRINK MENU')
     print('□■  1.코카콜라: 1,000원')
@@ -43,7 +51,7 @@ def choice_drink():
     print('■■ 원하시는 메뉴의 번호를 입력해주세요.')
     print('■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■')
 
-    while True:
+    while True: 
         choice_num3 = int(input('>> 번호: '))
         if choice_num3>= 1 and choice_num3 <= 3:
             break
